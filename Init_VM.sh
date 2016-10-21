d957e391ff7eefa162a90c514acee2c6bc8898b4
sridharpaladugu <spaladugu@pivotal.io>
2016-10-21 02:08:38 -0400

    update Init_VM with basic stuff

--- Init_VM.sh
+++ Init_VM.sh
@@ -1,7 +1,11 @@
-!/bin/sh
-sudo su -c "{ echo -n '`hostname -I`     '; echo -n '`hostname -f`     '; echo `hostname`; } >> /etc/hosts"
+#!/bin/sh
+sed -i "s/Defaults\s\{1,\}requiretty/Defaults \!requiretty/g" /etc/sudoers
 sudo su -c "echo 0 > /selinux/enforce"
-sudo su -c 'sed -i -e "s/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux"
+sudo su -c "sed -i -e 's/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux"
 sudo su -c "service iptables stop"
 sudo su -c "chkconfig iptables off"
 sudo su -c "chkconfig ip6tables off"
+sudo su -c "yum install -y ntp"
+sudo su -c "service ntp start"
+sudo su -c "chkconfig ntp on"
+
