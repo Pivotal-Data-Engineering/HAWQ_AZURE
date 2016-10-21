1d381149b54a552b4cacb250becd997a84cbf702
sridharpaladugu <spaladugu@pivotal.io>
2016-10-21 00:10:20 -0400

    Modify to correct ambari url and install option

--- Init_Ambari_Node.sh
+++ Init_Ambari_Node.sh
@@ -1,11 +1,11 @@
-!/bin/sh
+#!/bin/sh
 sudo su -c "{ echo -n '`hostname -I`     '; echo -n '`hostname -f`     '; echo `hostname`; } >> /etc/hosts"
 sudo su -c "echo 0 > /selinux/enforce"
-sudo su -c 'sed -i -e "s/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux"
+sudo su -c "sed -i -e 's/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux"
 sudo su -c "service iptables stop"
 sudo su -c "chkconfig iptables off"
 sudo su -c "chkconfig ip6tables off"
-sudo su -c "wget ‐‐directory-prefix=/etc/yum.repos.d http://public-repo-1.hortonworks.com/ambari/centos6/1.x/GA/ambari.repo"
+sudo su -c "wget http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.2.2.0/ambari.repo -P /etc/yum.repos.d/"
 sudo su -c "yum install ambari-server"
-sudo su -c "ambari-server -s setup"
+sudo su -c "ambari-server setup -s"
 sudo su -c "ambari-server start"
