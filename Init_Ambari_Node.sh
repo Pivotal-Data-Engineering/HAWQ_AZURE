0ba91acd6c60f19aa0c1c0ab86dec93f1bd6554f
sridharpaladugu <spaladugu@pivotal.io>
2016-10-21 01:11:35 -0400

    try change the tty option

--- Init_Ambari_Node.sh
+++ Init_Ambari_Node.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+sed -i "s/Defaults\s\{1,\}requiretty/Defaults \!requiretty/g" /etc/sudoers
 sudo su -c "echo 0 > /selinux/enforce"
 sudo su -c "sed -i -e 's/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux"
 sudo su -c "service iptables stop"
