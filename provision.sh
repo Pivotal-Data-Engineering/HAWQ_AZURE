c3374a8228ffd04b5f27f14437f4f24369a3c8b6
sridharpaladugu <spaladugu@pivotal.io>
2016-10-22 11:46:15 -0400

    clean up modulerize test

--- provision.sh
+++ provision.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
+figlet -f digital Pivotal Software 
+
 rgName=datalake01-rg
 
-templateFile=/Users/spaladugu/Desktop/Azure_HDP/hdb-dl-ambari-azure.json
+cleanupTemplateFile=/Users/spaladugu/DEV/github/HAWQ_AZURE/cleanup-all-resources.json
+deployTemplateFile=/Users/spaladugu/DEV/github/HAWQ_AZURE/hdb-dl-ambari-azure-1.json
 
-parameterFile=/Users/spaladugu/Desktop/Azure_HDP/hdb-dl-ambari-azure.parameters.json
+parameterFile=/Users/spaladugu/DEV/github/HAWQ_AZURE/hdb-dl-ambari-azure.parameters.json
 
 #echo 'Creating Azure resource group $rgName in region eastus....'
 
 #azure group create -n $rgName -l eastus
 
+echo 'Cleaning all previous resources from $rgName ...................'
+
+azure group deployment create -g $rgName -f $cleanupTemplateFile -m Complete  
 
 echo 'Running deployment in group $rgName using $templateFile and $parameterFile ......'
 
-azure group deployment create -d All -g $rgName -f $templateFile -e $parameterFile
+azure group deployment create -d All -g $rgName -f $deployTemplateFile -e $parameterFile
 
