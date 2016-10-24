67488deb7108eea2e52e0dd6c19b0788bbb09ba7
sridharpaladugu <spaladugu@pivotal.io>
2016-10-24 03:24:10 -0400

    change the init script for sshd

--- Init_Ambari_Node.sh
+++ Init_Ambari_Node.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 userName=$1
-sudo su -c "sed -i 's/Defaults\s\{1,\}requiretty/Defaults \!requiretty/g' /etc/sudoers"
+"sed -i 's/Defaults\s\{1,\}requiretty/Defaults \!requiretty/g' /etc/sudoers"
 sudo su -c "echo 0 > /selinux/enforce"
 sudo su -c "sed -i -e 's/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux"
 sudo su -c "service iptables stop"
@@ -13,6 +13,10 @@ sudo su -c "su - ${userName} -c 'ssh-keygen -f /home/${userName}/.ssh/id_rsa1 -t
 sudo su -c "su - ${userName} -c 'chmod 700 /home/${userName}/.ssh' "
 sudo su -c "su - ${userName} -c 'chmod 600 /home/${userName}/.ssh/authorized_keys' "
 sudo su -c "yum install -y expect"
+
+sudo su -c 'ssh-keygen -f /home/${userName}/.ssh/id_rsa1 -t rsa -N "'""'"'"
+sudo su -c "su - ${userName} -c 'chmod 700 /home/${userName}/.ssh' "
+sudo su -c "su - ${userName} -c 'chmod 600 /home/${userName}/.ssh/authorized_keys' "
 
 sudo su -c "wget http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.2.2.0/ambari.repo -P /etc/yum.repos.d/"
 sudo su -c "yum install -y ambari-server"
