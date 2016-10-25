84ad697a7c31c9a2e271030536f2e7264b197630
sridharpaladugu <spaladugu@pivotal.io>
2016-10-24 21:44:49 -0400

    remove sudo

--- Init_AmbariVM.sh
+++ Init_AmbariVM.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-userName=$1
+ADMINUSER=$1
 sed -i 's/Defaults\s\{1,\}requiretty/Defaults \!requiretty/g' /etc/sudoers
-sudo echo "$username    ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-sudo echo 0 > /selinux/enforce
-sudo sed -i -e 's/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux
-sudo service iptables stop
-sudo chkconfig iptables off
-sudo chkconfig ip6tables off
+echo "$ADMINUSER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo 0 > /selinux/enforce
+sed -i -e 's/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux
+service iptables stop
+chkconfig iptables off
+chkconfig ip6tables off
 sudo su -c "{ echo -n '`hostname -I`     '; echo -n '`hostname -f`     '; echo `hostname`; } >> /etc/hosts"
 
-sudo yum install -y ntp
-sudo chkconfig ntpd on
+yum install -y ntp
+chkconfig ntpd on
 
-sudo echo 'umask 0022' >> /etc/profile
+echo 'umask 0022' >> /etc/profile
 
-sudo wget http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.2.2.0/ambari.repo -P /etc/yum.repos.d/
-sudo yum install -y ambari-server
-sudo ambari-server setup -s
-sudo ambari-server start
+wget http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.2.2.0/ambari.repo -P /etc/yum.repos.d/
+yum install -y ambari-server
+ambari-server setup -s
+ambari-server start
 
 
 
