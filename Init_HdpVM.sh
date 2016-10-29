47e649e131ab4cbca4de7f00e2120184c9eb5169
sridharpaladugu <spaladugu@pivotal.io>
2016-10-29 00:00:09 -0400

    error handle subscript

--- Init_HdpVM.sh
+++ Init_HdpVM.sh
@@ -33,6 +33,7 @@ cp /home/$ADMINUSER/.ssh/authorized_keys /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 chmod 700 /root/.ssh
 
+echo "Peparing Disks.... "
 ./vm-disk-utils-centos.sh -s
 
 
